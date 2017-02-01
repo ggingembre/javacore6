c98923d06ab73109e006a2b771aa2fee4ef9a0ac
ggingembre@gmail.com <Sailing2010>
2017-02-01 13:13:14 +0200

    first commit

---
+++ src/FirstProgram.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Guillaume Gingembre on 01/02/2017.
+ */
+public class FirstProgram {
+    public static void main (String[] args){
+        System.out.println("First Program works");
+        System.out.println("done...");
+    }
+}
---
+++ src/test_webinar1.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Guillaume Gingembre on 01/02/2017.
+ */
+public class test_webinar1 {
+    public static void main (String[] args){
+    }
+}
